251e05e4c3e49d50304e779dc8d3d5fc4e47f578
dlicudi <dlicudi@gmail.com>
2026-03-19 11:21:35 +0100

    Gate XP weather representations on free-threaded Python

--- cockpitdecks_xp/buttons/representation/__init__.py
+++ cockpitdecks_xp/buttons/representation/__init__.py
@@ -1,2 +1,5 @@
-from .xpweather import XPRealWeatherMetarIcon
-from .xpstationplot import XPRealWeatherStationPlot
+import sysconfig
+
+if not sysconfig.get_config_var("Py_GIL_DISABLED"):
+    from .xpweather import XPRealWeatherMetarIcon
+    from .xpstationplot import XPRealWeatherStationPlot
